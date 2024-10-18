1094dadf3437802965765cb56346c1ba03e1c408
mechlin-sda-shahnawaaz <shahnawaaz.ansari@mechlintech.com>
2024-10-18 19:18:29 +0530

    Added Resend Otp feature if OTP Expired and Improve Code Quality

--- src/hooks/useSignUp.tsx
+++ src/hooks/useSignUp.tsx
@@ -5,8 +5,6 @@ import {
   setRefreshToken,
   setRole,
 } from "../Redux/Slice/auth.slice";
-import axios from "axios";
-import { baseURL } from "../env";
 import { IEmailInput, IOTPInput } from "../types/login.types";
 import useAPIs from "./useApi";
 import { useNavigate } from "react-router-dom";
@@ -20,6 +18,7 @@ const useSignUp = () => {
   const dispatch = useDispatch();
 
   const onSubmitEmail = async (data: IEmailInput) => {
+    localStorage.setItem("email", data.email);
     setEmail(data.email);
     const result = await makeAPICallWithData("post", "/users/sendOtp", {
       email: data.email,
@@ -30,35 +29,39 @@ const useSignUp = () => {
       return;
     }
     if (response.data.success) {
-      localStorage.setItem("email", data.email);
       localStorage.setItem("otpId", response.data?.data.id);
-      toast("Otp sent successfully");
+      toast("Otp sent successfully !!");
       setStep("otp");
     }
   };
 
   const onSubmitOTP = async (data: IOTPInput) => {
-    const resp = await axios.post(`${baseURL}/users/validateOtp`, {
+    const {
+      isError,
+      response: resp,
+      error,
+    } = await makeAPICallWithData("post", "/users/validateOtp", {
       otp: data.otp,
       id: localStorage.getItem("otpId"),
       email: localStorage.getItem("email"),
     });
-
-    if (resp.data.success) {
-      const { accessToken, refreshToken } = resp.data;
-      dispatch(setAccessToken({ accessToken }));
-      dispatch(setRefreshToken({ refreshToken }));
-      dispatch(setRole({ role: resp.data.role }));
-      if (!resp.data.newUser) {
-        localStorage.removeItem("otpId");
-        localStorage.removeItem("email");
-        navigate("/");
-        return;
-      }
-      setStep("details");
-    } else {
-      alert(resp.data.message);
+    if (isError || !resp) {
+      toast.error(error?.response?.data?.message || "Something went Wrong !!");
+      return;
     }
+
+    const { accessToken, refreshToken } = resp.data;
+    dispatch(setAccessToken({ accessToken }));
+    dispatch(setRefreshToken({ refreshToken }));
+    dispatch(setRole({ role: resp.data.role }));
+    toast.success("Login Successfully !!");
+    if (!resp.data.newUser) {
+      localStorage.removeItem("otpId");
+      localStorage.removeItem("email");
+      navigate("/");
+      return;
+    }
+    setStep("details");
   };
 
   return {

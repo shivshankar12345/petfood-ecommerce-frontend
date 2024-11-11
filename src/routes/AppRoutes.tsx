7249aa5dc70143ec3a907d140c321225e40dc508
Satyam-Mechlintech <Satyam.sitoliwal@mechlintech.com>
2024-11-11 12:08:45 +0530

    work on the track order and return order Page

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -5,6 +5,8 @@ import SignUpPage from "../pages/user/SignUpPage";
 import CartPage from "../pages/CartPage";
 import PincodeModal from "../pages/PincodePage";
 import LogOutPage from "../pages/user/LogOutPage";
+import ReturnOrder from "../pages/Contact/ReturnOrder";
+import TrackOrder from "../pages/Contact/TrackOrder";
 
 const AppRoutes: React.FC = () => {
   return (
@@ -24,6 +26,8 @@ const AppRoutes: React.FC = () => {
       <Route path="signup" element={<SignUpPage />} />
       <Route path="signout" element={<LogOutPage />} />
       <Route path="cart" element={<CartPage />} />
+      <Route path="/track-order" element={<TrackOrder></TrackOrder>} />
+      <Route path="/return-order" element={<ReturnOrder></ReturnOrder>} />
     </Routes>
   );
 };
